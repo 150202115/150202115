6aaa9eb34d6fdd6a314cbe67bd98635a5b1a6f5d
150202115 <150202115@kocaeli.edu.tr>
2019-12-26 23:18:36 +0300

    first commit

---
+++ ilac.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ilac',
+  templateUrl: './ilac.page.html',
+  styleUrls: ['./ilac.page.scss'],
+})
+export class IlacPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
